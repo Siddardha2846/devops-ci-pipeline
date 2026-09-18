0f525b602288b8bc18d83a5afb59e9a7b77dc63c
Siddardha2846 <tsiddardha0@gmail.com>
2026-09-18 22:44:33 +0530

    Initial DevOps CI pipeline project

---
+++ src/test/java/devops/AppTest.java
@@ -0,0 +1,12 @@
+package devops;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testHelloDevOps() {
+        assertEquals("Hello DevOps", "Hello DevOps");
+    }
+}
